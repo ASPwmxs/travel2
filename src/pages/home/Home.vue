0e6246bf1ecb81ec8bae270f46790355636aebbf
ASPwmxs <2433967588@qq.com>
2019-07-08 16:22:33 +0800

    'projectInit'

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    hello world
+    <!-- <router-link to="/list" class="home">列表页</router-link> -->
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
