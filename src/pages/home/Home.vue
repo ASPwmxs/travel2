d223d390a84fe20f274aae8c994185a49b505b43
ASPwmxs <2433967588@qq.com>
2019-07-11 20:22:33 +0800

    'change'

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,19 @@
 <template>
-<div>
-    <home-header></home-header>
-      <!-- <router-link to="/list" class="home">列表页</router-link> -->
-</div>
+  <div>
+      <home-header></home-header>
+      <home-swiper></home-swiper>
+        <!-- <router-link to="/list" class="home">列表页</router-link> -->
+  </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
