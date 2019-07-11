f220f5e0a51894df18a8564a8a56b943395bc252
ASPwmxs <2433967588@qq.com>
2019-07-11 21:12:12 +0800

    'iconupdate1'

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="icons">
+        <div class="icon">
+            <div class="icon-img">
+                <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/4d/a1eda1a2b8414302.png" />
+            </div>
+            <p class="icon-desc">暑假夜场</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/4d/a1eda1a2b8414302.png" />
+            </div>
+            <p class="icon-desc">暑假夜场</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/4d/a1eda1a2b8414302.png" />
+            </div>
+            <p class="icon-desc">暑假夜场</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/4d/a1eda1a2b8414302.png" />
+            </div>
+            <p class="icon-desc">暑假夜场</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/4d/a1eda1a2b8414302.png" />
+            </div>
+            <p class="icon-desc">暑假夜场</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/4d/a1eda1a2b8414302.png" />
+            </div>
+            <p class="icon-desc">暑假夜场</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/4d/a1eda1a2b8414302.png" />
+            </div>
+            <p class="icon-desc">暑假夜场</p>
+        </div>
+        <div class="icon">
+            <div class="icon-img">
+                <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/4d/a1eda1a2b8414302.png" />
+            </div>
+            <p class="icon-desc">暑假夜场</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .icons
+      overflow:hidden
+      height:0
+      padding-bottom:50%
+      background:lightblue
+      .icon
+          position:relative
+          width:25%
+          height:0
+          float:left
+          padding-bottom:24%
+          .icon-img
+            position:absolute
+            top:0
+            left:0
+            right:0
+            bottom:.44rem
+            box-sizing:border-box
+            padding:.1rem
+            .icon-img-content
+                display:block
+                margin:0 auto
+                height:100%
+          .icon-desc
+            position absolute
+            left:0
+            right:0
+            bottom:0
+            height:.44rem
+            line-height:.44rem
+            text-align:center
+            color:$darkTextColor
+</style>
